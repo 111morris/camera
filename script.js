9361b153b1d2ab2e7f1aec94e4dff505b15abcaa
mulandi <mulandimorris1@gmail.com>
2025-07-06 22:10:24 +0300

    Added logic to camera to capture and save as png

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cameraFeed = document.querySelector("#cameraFeed");
+/* const cameraFeed = document.querySelector("#cameraFeed");
 navigator.mediaDevices.getUserMedia({video: true})
  .then(stream => {
   cameraFeed.srcObject = stream;
@@ -6,4 +6,26 @@ navigator.mediaDevices.getUserMedia({video: true})
 
  .catch(error => {
   console.error("cant open it");
- })
+ }) */
+
+
+const video = document.querySelector("#video");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const captureBtn = document.querySelector("#captureBtn");
+const downloadLink = document.querySelector("#downloadLink");
+
+// this will start the webcam
+navigator.mediaDevices.getUserMedia({ video: true })
+ .then(stream => {
+  video.srcObject = stream;
+ }).catch(err => {
+  console.error("Camera access failed:", err);
+ });
+
+// this will capture the frame
+captureBtn.addEventListener("click", () => {
+ ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+ const image = canvas.toDataURL("image/png");
+ downloadLink.href = image;
+});
